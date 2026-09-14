0b943ff5eb6c8eab618f79fe25cfc422632704e7
bucketdeed <87669671+bucketdeed@users.noreply.github.com>
2026-09-14 19:28:34 +0100

    material: remove gki warning

--- manager/app/src/main/java/me/weishu/kernelsu/ui/screen/home/HomeMaterial.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/screen/home/HomeMaterial.kt
@@ -108,8 +108,8 @@ fun HomePagerMaterial(
                     )
                 )
             }
-            if (state.showGkiWarning) {
-                WarningCard(stringResource(id = R.string.home_gki_warning), level = WarningLevel.Notice)
+            //if (state.showGkiWarning) {
+                //WarningCard(stringResource(id = R.string.home_gki_warning), level = WarningLevel.Notice)
             }
             if (state.showUAPIMisMatchWarning) {
                 WarningCard(
